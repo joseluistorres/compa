262ac2c63c58e53ff937a092c09e9ff2d1b1a071
JoseLuis Torres <jl@joseluistorres.me>
2017-03-03 10:51:37 -0600

    make it better

--- web.go
+++ web.go
@@ -6,7 +6,10 @@ import (
   "net/http"
   "strconv"
   re "gopkg.in/gorethink/gorethink.v3"
-  "time"
+  // "time"
+  // "net/http/httputil"
+  // "fmt"
+  // "os"
 )
 
 type WebhookResponse struct {
@@ -18,8 +21,21 @@ var (
   session *re.Session
 )
 
+// func debug(data []byte, err error) {
+//     if err == nil {
+//         fmt.Printf("%s\n\n", data)
+//     } else {
+//         log.Fatalf("%s\n\n", err)
+//     }
+// }
+
 func init() {
   http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+    // data, err := httputil.DumpRequestOut(r, true)
+    // log.Printf("%s\n\n", data)
+    // if err != nil {
+    //     log.Fatal(err)
+    //   }
     incomingText := r.PostFormValue("text")
     if incomingText != "" && r.PostFormValue("user_id") != "" {
       text := parseText(incomingText)
@@ -32,15 +48,15 @@ func init() {
         response.Text = "This is just a simulated response"
         log.Printf("Sending response: %s", response.Text)
         // testing the rethinkDB
-        share := NewShare("https://www.rethinkdb.com/docs/reql-data-exploration/", incomingText)
-        share.Created = time.Now()
+        // share := NewShare("https://www.rethinkdb.com/docs/reql-data-exploration/", incomingText)
+        // share.Created = time.Now()
 
-        // Insert the new item into the database
-        _, err := re.Table("shares").Insert(share).RunWrite(session)
-        if err != nil {
-          http.Error(w, err.Error(), http.StatusInternalServerError)
-          return
-        }
+        // // Insert the new item into the database
+        // _, err := re.Table("shares").Insert(share).RunWrite(session)
+        // if err != nil {
+        //   http.Error(w, err.Error(), http.StatusInternalServerError)
+        //   return
+        // }
 
         b, err := json.Marshal(response)
         if err != nil {
@@ -64,16 +80,16 @@ func StartServer(port int) {
 }
 
 func initDB() {
-  var err error
+  //var err error
 
-  session, err = re.Connect(re.ConnectOpts{
-    Address: "127.0.0.1:28015",
-    Database: os.Getenv("RETHINKDB_DATABASE"),
-    Username: os.Getenv("RETHINKDB_USERNAME"),
-    Password: os.Getenv("RETHINKDB_PASSWORD"),
-  })
+  // session, err = re.Connect(re.ConnectOpts{
+  //   Address: "127.0.0.1:28015",
+  //   Database: os.Getenv("RETHINKDB_DATABASE"),
+  //   Username: os.Getenv("RETHINKDB_USERNAME"),
+  //   Password: os.Getenv("RETHINKDB_PASSWORD"),
+  // })
 
-  if err != nil {
-    log.Fatalln(err.Error())
-  }
+  // if err != nil {
+  //   log.Fatalln(err.Error())
+  // }
 }
